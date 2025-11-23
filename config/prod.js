2c8a501745c0418643f06003b4193ead937091f1
Tsoorh <Tsoor.hartov@gmail.com>
2025-11-23 14:05:45 +0200

    minor change at - prod.js for production security

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,12 @@
 
 // for checking prod 
+
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default {
-    dbURL:process.env.MONGO_URL || "mongodb+srv://tsoorh:Tsoorielks12@cluster0.ilmnnlj.mongodb.net/",
-    dbName:process.env.DB_NAME ||'bug_db'
+    dbURL:process.env.MONGO_URL,
+    dbName:process.env.DB_NAME 
 }
 
 
